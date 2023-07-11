39d117a2c270d2af4c5232fab4bf23ea7a38f197
高雷 <gaolei@wellsign.cn>
2023-07-11 15:44:36 +0800

    Add view jitter and flicker extension functions

--- ktx/src/main/java/com/gc/ktx/UI.kt
+++ ktx/src/main/java/com/gc/ktx/UI.kt
@@ -3,6 +3,10 @@ package com.gc.ktx
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.view.animation.LinearInterpolator
 
 fun View.gone() {
     visibility = GONE
@@ -20,3 +24,18 @@ fun View.visible(visible: Boolean) {
     visibility = if (visible) VISIBLE else GONE
 }
 
+fun View.jitter() {
+    val animation = AnimationUtils.loadAnimation(context, R.anim.shake)
+    startAnimation(animation)
+}
+
+fun View.flicker() {
+    val animation = AlphaAnimation(1f, 0f)
+    animation.duration = 500
+    animation.interpolator = LinearInterpolator()
+    animation.repeatCount = Animation.INFINITE
+    animation.repeatMode = Animation.REVERSE
+    startAnimation(animation)
+}
+
+fun View.stopFlicker() = clearAnimation()
